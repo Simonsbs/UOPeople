695fd710e5148e2128047ca25d1a774cf6357b2f
Simon B.Stirling <simonsbs@gmail.com>
2023-12-17 22:46:57 +0200

    .

--- CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/Unit5/Student.java
+++ CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/Unit5/Student.java
@@ -1,7 +1,105 @@
 package Unit5;
 
+import java.util.ArrayList;
+
+/**
+ * The Student class represents a student in a university.
+ * It stores the student's name, ID, and the courses they are enrolled in,
+ * along with the grades for each course.
+ */
 public class Student {
     private String name;
     private String ID;
+    private ArrayList<Course> enrolledCourses;
+    private ArrayList<String> grades;
 
+    /**
+     * Constructor for the Student class.
+     *
+     * @param name the name of the student.
+     * @param ID   the unique ID of the student.
+     */
+    public Student(String name, String ID) {
+        this.name = name;
+        this.ID = ID;
+        this.enrolledCourses = new ArrayList<>();
+        this.grades = new ArrayList<>();
+    }
+
+    /**
+     * Enrolls the student in a course.
+     *
+     * @param course the course to enroll in.
+     */
+    public void enrollInCourse(Course course) {
+        enrolledCourses.add(course);
+        grades.add(""); // Placeholder for grade
+    }
+
+    /**
+     * Assigns a grade to the student for a specific course.
+     *
+     * @param course the course to assign the grade to.
+     * @param grade  the grade to assign.
+     */
+    public void assignGrade(Course course, String grade) {
+        int courseIndex = enrolledCourses.indexOf(course);
+        if (courseIndex != -1) {
+            grades.set(courseIndex, grade);
+        }
+    }
+
+    /**
+     * Gets the student's name.
+     *
+     * @return the name of the student.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the student's name.
+     *
+     * @param name the name to set for the student.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the student's ID.
+     *
+     * @return the ID of the student.
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * Sets the student's ID.
+     *
+     * @param ID the ID to set for the student.
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * Gets a list of courses the student is enrolled in.
+     *
+     * @return the list of enrolled courses.
+     */
+    public ArrayList<Course> getEnrolledCourses() {
+        return new ArrayList<>(enrolledCourses); // Return a copy to prevent external modification
+    }
+
+    /**
+     * Gets the grades of the student for their courses.
+     *
+     * @return the list of grades for each enrolled course.
+     */
+    public ArrayList<String> getGrades() {
+        return new ArrayList<>(grades); // Return a copy to prevent external modification
+    }
 }
