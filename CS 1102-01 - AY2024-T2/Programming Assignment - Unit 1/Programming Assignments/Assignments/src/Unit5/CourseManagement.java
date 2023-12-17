48ccc39f5acf49478bdbcca69801aeb9c5b49002
Simon B.Stirling <simonsbs@gmail.com>
2023-12-17 22:21:33 +0200

    .

--- CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/Unit5/CourseManagement.java
+++ CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/Unit5/CourseManagement.java
@@ -1,5 +1,11 @@
 package Unit5;
 
-public class CourseManagement {
+import java.util.ArrayList;
 
+public class CourseManagement {
+    private static ArrayList<Course> courses = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.print("...");
+    }
 }
