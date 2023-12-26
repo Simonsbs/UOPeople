c7079a4890068f1182b0fc630d5ce4e84f10ea11
Simon B.Stirling <simonsbs@gmail.com>
2023-12-26 23:12:09 +0200

    .

--- CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/unit6/utilities/SampleData.java
+++ CS 1102-01 - AY2024-T2/Programming Assignment - Unit 1/Programming Assignments/Assignments/src/unit6/utilities/SampleData.java
@@ -38,27 +38,69 @@ public class SampleData {
      * @return a list of all sample spacecrafts
      */
     public static void getAllSamples(ArrayList<Spacecraft> list) {
-        list.add(new Starfighter("X-Wing", "T-65", "Incom 4L4 Fusial Thrust Engine", true, RandomDouble(),
-                "Laser Cannons", true));
-        list.add(new Starfighter("TIE Fighter", "Twin Ion Engine LN Starfighter", "Sienar Fleet Systems", true,
+        list.add(new Starfighter(
+                "X-Wing",
+                "T-65",
+                "Incom 4L4 Fusial Thrust Engine",
+                true,
+                RandomDouble(),
+                "Laser Cannons",
+                true));
+        list.add(new Starfighter("TIE Fighter",
+                "Twin Ion Engine LN Starfighter",
+                "Sienar Fleet Systems",
+                true,
                 RandomDouble(),
                 "Laser Cannons",
                 false));
-        list.add(new Starfighter("A-Wing", "RZ-1", "Alliance Underground Engineering", true, RandomDouble(),
+        list.add(new Starfighter("A-Wing",
+                "RZ-1",
+                "Alliance Underground Engineering",
+                true,
+                RandomDouble(),
                 "Laser Cannons",
                 true));
-        list.add(new Shuttle("Galaxy Class", "NCC-1701-D", "Warp Drive", true, RandomDouble(), 1014,
+        list.add(new Shuttle("Galaxy Class",
+                "NCC-1701-D",
+                "Warp Drive",
+                true,
+                RandomDouble(),
+                1014,
                 "Exploration and Diplomacy"));
-        list.add(new Shuttle("Imperial Shuttle", "Lambda-class T-4a", "Cygnus Spaceworks", true, RandomDouble(), 20,
+        list.add(new Shuttle("Imperial Shuttle",
+                "Lambda-class T-4a",
+                "Cygnus Spaceworks",
+                true,
+                RandomDouble(),
+                20,
                 "Troop Transport"));
-        list.add(new Shuttle("Runabout", "Danube Class", "Warp Drive", true, RandomDouble(), 40,
+        list.add(new Shuttle("Runabout",
+                "Danube Class",
+                "Warp Drive",
+                true,
+                RandomDouble(),
+                40,
                 "Short-range Missions"));
-        list.add(new CargoShip("Nostromo", "Lockmart CM-88B Bison M-Class Starfreighter",
-                "Rolls-Royce N66 Cyclone Thrust Tunnels", true, RandomDouble(), 20000, true));
-        list.add(new CargoShip("Sulaco", "Conestoga-class Troop Transport", "Westingland A24", true, RandomDouble(),
-                50000, true));
-        list.add(new CargoShip("Valley Forge", "American Airlines Space Freighter", "General Electric CF6-50", true,
-                RandomDouble(), 100000,
+        list.add(new CargoShip("Nostromo",
+                "Lockmart CM-88B Bison M-Class Starfreighter",
+                "Rolls-Royce N66 Cyclone Thrust Tunnels",
+                true,
+                RandomDouble(),
+                20000,
+                true));
+        list.add(new CargoShip("Sulaco",
+                "Conestoga-class Troop Transport",
+                "Westingland A24",
+                true,
+                RandomDouble(),
+                50000,
+                true));
+        list.add(new CargoShip("Valley Forge",
+                "American Airlines Space Freighter",
+                "General Electric CF6-50",
+                true,
+                RandomDouble(),
+                100000,
                 false));
     }
 }
